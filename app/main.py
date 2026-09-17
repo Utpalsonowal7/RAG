2d8d36010089e4b5c39a473ecb9f6f8361414fc4
Utpal Sonowal <sonowalu73@gmail.com>
2026-09-17 12:38:00 +0530

    fixed the cors

--- app/main.py
+++ app/main.py
@@ -1,15 +1,30 @@
-from fastapi import FastAPI,APIRouter
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.doc import router as upload_docs
 
 app = FastAPI()
 
-api_router=APIRouter(prefix="/api")
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+api_router = APIRouter(prefix="/api")
 
 api_router.include_router(upload_docs)
 
 app.include_router(api_router)
 
+
 @app.get("/")
 def root():
-     return {"status : ok"}
+    return {"status : ok"}
